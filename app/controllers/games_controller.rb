e0161300fedb3353adc9b4439b4ec4d1955e577f
Karel Asselman <asselman.karel@gmail.com>
2020-11-02 14:41:19 +0100

    completed score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+    session[:score] ||= 0
+  end
+
+  def score
+    word = params[:word].upcase
+    letters = params[:letters].split(' ')
+    word.each_char do |letter|
+      if letters.include?(letter)
+        letters.delete_at(letters.index(letter))
+      else
+        @message = "Sorry but #{word} cannot be built out of #{letters}"
+      end
+    end
+    unless @message
+      @message = "Sorry but #{word} does not seems to be a valid English word..." unless valid_word?(word)
+      session[:score] += word.length
+    end
+    @message ||= "Congratulations! #{word} is a valid English word!"
+  end
+
+  def valid_word?(word)
+    response = JSON.parse(RestClient.get("https://wagon-dictionary.herokuapp.com/#{word}"))
+    response['found']
+  end
+end
